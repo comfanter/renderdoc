75b20535ae3879bafaa9446e62c97bdea0306329
baldurk <baldurk@baldurk.org>
2017-12-22 20:55:22 +0000

    test9

--- scripts/travis/docs_compile.sh
+++ scripts/travis/docs_compile.sh
@@ -11,10 +11,12 @@ mkdir build
 pushd build
 
 # Do a minimal build with as little as possible to get the python modules
-CC=gcc-6 CXX=g++-6 cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_GL=OFF -DENABLE_VULKAN=OFF -DENABLE_RENDERDOCCMD=OFF -DENABLE_QRENDERDOC=OFF ..
+CC=clang CXX=clang++ cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_GL=OFF -DENABLE_VULKAN=OFF -DENABLE_RENDERDOCCMD=OFF -DENABLE_QRENDERDOC=OFF ..
 make -j2
 
 cat CMakeCache.txt
+
+cat qrenderdoc/custom_swig-prefix/src/custom_swig/config.log
 
 /usr/bin/python3 --version
 
