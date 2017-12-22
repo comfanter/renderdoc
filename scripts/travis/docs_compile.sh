02adaf818d813abf2a3c2466712b885959a728e4
baldurk <baldurk@baldurk.org>
2017-12-22 18:26:01 +0000

    Attempt to get a callstack out of gdb from trying to import modules

--- scripts/travis/docs_compile.sh
+++ scripts/travis/docs_compile.sh
@@ -10,9 +10,32 @@ set -ev
 mkdir build
 pushd build
 
+which python3
+ls -l $(which python3)
+file $(which python3)
+
+cat $(which python3)
+
+echo "print('foo')" > t est.py
+echo -e "run\nbt" > gdb.cmd
+gdb --batch --command=gdb.cmd --args python3 test.py
+
 # Do a minimal build with as little as possible to get the python modules
 CC=gcc-6 CXX=g++-6 cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_GL=OFF -DENABLE_VULKAN=OFF -DENABLE_RENDERDOCCMD=OFF -DENABLE_QRENDERDOC=OFF ..
 make -j2
+
+wc -l qrenderdoc/renderdoc_python.cxx
+wc -l qrenderdoc/qrenderdoc_python.cxx
+
+cd bin
+
+echo -e "import renderdoc\nprint(dir(renderdoc))" > test.py
+echo -e "run\nbt" > gdb.cmd
+gdb --batch --command=gdb.cmd --args python3 test.py
+
+echo -e "import qrenderdoc\nprint(dir(qrenderdoc))" > test.py
+echo -e "run\nbt" > gdb.cmd
+gdb --batch --command=gdb.cmd --args python3 test.py
 
 popd
 
