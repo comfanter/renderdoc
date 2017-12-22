a65b310f2c446bb536a2a679d6d8ba44f3082237
baldurk <baldurk@baldurk.org>
2017-12-22 20:34:27 +0000

    test8

--- scripts/travis/docs_compile.sh
+++ scripts/travis/docs_compile.sh
@@ -14,6 +14,10 @@ pushd build
 CC=gcc-6 CXX=g++-6 cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_GL=OFF -DENABLE_VULKAN=OFF -DENABLE_RENDERDOCCMD=OFF -DENABLE_QRENDERDOC=OFF ..
 make -j2
 
+cat CMakeCache.txt
+
+/usr/bin/python3 --version
+
 wc -l qrenderdoc/renderdoc_python.cxx
 wc -l qrenderdoc/qrenderdoc_python.cxx
 
@@ -23,8 +27,10 @@ echo "run" > gdb.cmd
 echo "bt" >> gdb.cmd
 echo "info proc mappings" >> gdb.cmd
 echo "p &PyType_Type" >> gdb.cmd
+echo "p &PyType_Type.tp_basicsize" >> gdb.cmd
 echo "p PyType_Type" >> gdb.cmd
 echo "p PyType_Type.tp_basicsize" >> gdb.cmd
+echo "info reg" >> gdb.cmd
 echo "disassemble /m" >> gdb.cmd
 
 echo -e "import renderdoc\nprint(dir(renderdoc))" > test.py
