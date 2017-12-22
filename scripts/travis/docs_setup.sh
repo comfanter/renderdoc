675cdc6169c5f22005a2136512af28059346e7f4
baldurk <baldurk@baldurk.org>
2017-12-22 19:05:05 +0000

    test5

--- scripts/travis/docs_setup.sh
+++ scripts/travis/docs_setup.sh
@@ -1,22 +1,12 @@
 #!/bin/sh
 set -ev
 
-echo "Outside env"
-
-env
-
-echo "Inside env:"
-
-echo -e "import sys\nimport os\n\nprint(sys.executable)\nprint(os.environ)" |  python3
-
-exit
-
 sudo add-apt-repository -y 'ppa:ubuntu-toolchain-r/test'
 sudo add-apt-repository -y 'ppa:beineri/opt-qt562-trusty'
 sudo add-apt-repository -y 'deb http://apt.llvm.org/precise/ llvm-toolchain-precise-3.8 main'
 wget -O - http://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
 sudo apt-get update -qq
-sudo apt-get install --allow-unauthenticated -y -qq libx11-dev mesa-common-dev libgl1-mesa-dev qt56base libxcb-keysyms1-dev gdb g++-6 python3-pip
+sudo apt-get install --allow-unauthenticated -y libx11-dev mesa-common-dev libgl1-mesa-dev qt56base libxcb-keysyms1-dev gdb g++-6 python3-pip
 
 sudo pip3 install --upgrade pip setuptools
 sudo pip3 install Sphinx sphinx-rtd-theme
